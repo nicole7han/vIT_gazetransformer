e21b8f93c8b231ddd0fcc4db122570a07a864d99
nicole7han <nicole7han@gmail.com>
2022-07-09 13:21:54 -0700

    update

--- evaluate_models/evaluate_viu.py
+++ evaluate_models/evaluate_viu.py
@@ -3,11 +3,12 @@ from script.model import *
 from statsmodels.formula.api import ols
 from evaluate_models.utils_fine_tuning import *
 from functions.data_ana_vis import *
+from script.matcher import *
 
 basepath = '/Users/nicolehan/Documents/Research/gazetransformer'
 model = Gaze_Transformer()
-epoch=200
-checkpoint = torch.load('trainedmodels/model_head/model_epoch{}.pt'.format(epoch), map_location='cpu')
+epoch=230
+checkpoint = torch.load('trainedmodels/model_head_chong_detr/model_epoch{}.pt'.format(epoch), map_location='cpu')
 plt.plot(checkpoint['train_loss'])
 # checkpoint['test_loss']
 loaded_dict = checkpoint['model_state_dict']
@@ -18,101 +19,101 @@ adapted_dict = {k[n_clip:]: v for k, v in loaded_dict.items()
 model.load_state_dict(adapted_dict)
 model.to(device)
 
-# #fine tuning model on viu dataset
-# train(e_start, num_e, anno_path, train_img_path, train_bbx_path, test_img_path, test_bbx_path)
 
-# # evaluate model on viu dataset
-# model = Gaze_Transformer()
-# checkpoint = torch.load('models/model_epoch{}.pt'.format(epoch), map_location='cpu')
-# model.load_state_dict(checkpoint['model_state_dict'])
-# model.to(device)
 # output = evaluate_model_gaze(anno_path, test_img_path, test_bbx_path, model, fig_path)
 # output.to_excel('gaze_video_data/model_epoch{}_result.xlsx'.format(epoch))
 
 # evaluate both models' estimation on viu dataset
 datapath = "/Users/nicolehan/Documents/Research/gazetransformer/gaze_video_data"
+outpath = '/Users/nicolehan/Documents/Research/gazetransformer'
 anno_path = '{}/Video_Info.xlsx'.format(datapath)
-# test_img_path = "{}/transformer_all_img_intact".format(datapath)
-# test_bbx_path = "{}/transformer_all_bbx".format(datapath)
+cond = 'nb'
+test_img_path = "{}/transformer_all_img_{}".format(datapath,cond)
+test_bbx_path = "{}/transformer_all_bbx".format(datapath)
+if cond == 'intact':
+    gazer_bbox = 'hb' # indicate what bounding box needs to feed into the model
+elif cond == 'nb':
+    gazer_bbox = 'h'
+elif cond == 'nh':
+    gazer_bbox = 'b'
 # train_img_path='gaze_video_data/transformer_train_img'
 # train_bbx_path='gaze_video_data/transformer_train_bbx'
-test_img_path='{}/transformer_all_img_intact'.format(datapath)
-test_bbx_path='{}/transformer_all_bbx'.format(datapath)
-if 'nb' in test_img_path: cond='nb'
-elif 'nh' in test_img_path : cond='nh'
-else: cond='intact'
-head_bbx_path='{}/boundingbox_head (gaze-orienting people)'.format(datapath)
-fig_path='{}/viT_epoch{}_{}_outputs'.format(datapath,epoch,cond)
+# test_img_path='{}/transformer_all_img_intact'.format(datapath)
+# test_bbx_path='{}/transformer_all_bbx'.format(datapath)
+# bbx_path='{}/boundingbox_head (gaze-orienting people)'.format(datapath)
+fig_path='{}/model_eval_viu_outputs/transformer_TRAINhb_TEST{}_epoch{}'.format(outpath,cond, epoch)
+
+matcher = build_matcher(set_cost_class=1, set_cost_bbox=5, set_cost_giou=2)
+weight_dict = {'loss_ce': 1, 'loss_bbox': 20, 'loss_giou': 2}
+losses = ['labels', 'boxes']
+num_classes = 1
+criterion = SetCriterion(num_classes, matcher=matcher, weight_dict=weight_dict,
+                         eos_coef=0.01, losses=losses)
+chong_est = pd.read_csv('{}/chong_estimation.csv'.format(outpath))
+transformer_est = evaluate_2model(anno_path, test_img_path, test_bbx_path, chong_est, model, fig_path, criterion,
+                    bbx_noise=False, gazer_bbox='hb')
+
+output.to_excel('{}/model_eval_viu_outputs/transformer_TRAINhb_TEST{}_epoch{}_result.xlsx'.format(outpath,cond,epoch), index=None)
+analyze_error(output, epoch, path='{}/model_eval_viu_outputs'.format(outpath))
 
 
-try:
- chong_est = pd.read_excel('{}/Chong_model_estimation.xlsx'.format(datapath))
- output = evaluate_2model(anno_path, test_img_path, test_bbx_path, head_bbx_path, chong_est, model, fig_path,
-                          bbx_noise=False)
-except:
- output = evaluate_2model(anno_path, test_img_path, test_bbx_path, head_bbx_path, None, model, fig_path,
-                          bbx_noise=False)
-output.to_excel('{}/chong&transformer_epoch{}_{}_result.xlsx'.format(datapath,epoch,cond), index=None)
-analyze_error(output, epoch, filename=datapath)
-
-
-## compare intact, floating heads and headless bodies
-intact = pd.read_excel('{}/chong&transformer_epoch{}_intact_result.xlsx'.format(datapath,epoch))
-fh = pd.read_excel('{}/chong&transformer_epoch{}_nb_result.xlsx'.format(datapath,epoch))
-hb = pd.read_excel('{}/chong&transformer_epoch{}_nh_result.xlsx'.format(datapath,epoch))
-intact['condition'] = 'intact'
-fh['condition'] = 'floating heads'
-hb['condition'] = 'headless bodies'
-alldata = pd.concat([intact, fh, hb])
-alldata['transformer_eucli_error'] = np.sqrt((alldata['gazed_x'] - alldata['transformer_est_x']) ** 2 + (
-         alldata['gazed_y'] - alldata['transformer_est_y']) ** 2)
-alldata['transformer_ang_error'] = alldata.apply(lambda row: compute_angle(row, 'transformer'), axis=1)
-
-#alldata['chong_eucli_error'] = np.sqrt( (alldata['gazed_x']-alldata['chong_est_x'])**2 + (alldata['gazed_y']-alldata['chong_est_y'])**2 )
-#alldata['chong_ang_error'] = alldata.apply(lambda row: compute_angle(row, 'chong'), axis=1)
-
-alldata['center_est_x'] = .5
-alldata['center_est_y'] = .5
-alldata['center_eucli_error'] =  np.sqrt((alldata['gazed_x'] - .5) ** 2 + (
-         alldata['gazed_y'] - .5) ** 2)
-alldata['center_ang_error'] = alldata.apply(lambda row: compute_angle(row, 'center'), axis=1)
-
-
-plotdata = alldata[['condition','transformer_eucli_error','center_eucli_error']]
-plotdata = pd.melt(plotdata,id_vars=['condition'],
-                   value_name='error')
-          
-sns_setup(sns)
-model = ols('error ~ C(condition)*C(variable)', data=plotdata).fit()
-ax = sns.barplot(data=plotdata, x='condition', y='error',hue='variable')
-ax.set(xlabel='', ylabel='Euclidean Error')
-change_width(ax,.4)
-ax.spines['top'].set_color('white')
-ax.spines['right'].set_color('white')
-ax.figure.savefig('{}/Figures/error_epoch{}.jpg'.format(datapath,epoch))
-
-
-plotdata = alldata[['condition','transformer_ang_error', 'center_ang_error']]
-plotdata = pd.melt(plotdata,id_vars=['condition'],
-                   value_name='error')
-       
-sns_setup(sns)
-ax = sns.barplot(data=plotdata, x='condition', y='error', hue='variable')
-ax.set(xlabel='', ylabel='Angular Error')
-change_width(ax,.4)
-ax.spines['top'].set_color('white')
-ax.spines['right'].set_color('white')
-ax.figure.savefig('{}/Figures/angerror_epoch{}.jpg'.format(datapath,epoch))
-
-
-# # test on the test images from large dataset
- datapath = os.path.abspath(os.curdir)
- anno_path = "{}/data/annotations".format(datapath)
- test_img_path = "{}/data/test".format(datapath)
- test_bbx_path = "{}/data/test_bbox".format(datapath)
- fig_path = '{}/model_eval_outputs_small/resnet_epoch{}_eval_outputs'.format(datapath, epoch)
- criterion = nn.MSELoss(reduction='mean')
- chong_est = pd.read_excel('{}/data/Chong_estimation_test.xlsx'.format(datapath))
- output = evaluate_test(anno_path, test_img_path, test_bbx_path, chong_est, criterion,  model, fig_path)
- output.to_excel('{}/model_eval_outputs_small/transformer_epoch{}_result.xlsx'.format(datapath, epoch))
- analyze_error(output, epoch, filename='figures/model_largedata')
+# ## compare intact, floating heads and headless bodies
+# intact = pd.read_excel('{}/chong&transformer_epoch{}_intact_result.xlsx'.format(datapath,epoch))
+# fh = pd.read_excel('{}/chong&transformer_epoch{}_nb_result.xlsx'.format(datapath,epoch))
+# hb = pd.read_excel('{}/chong&transformer_epoch{}_nh_result.xlsx'.format(datapath,epoch))
+# intact['condition'] = 'intact'
+# fh['condition'] = 'floating heads'
+# hb['condition'] = 'headless bodies'
+# alldata = pd.concat([intact, fh, hb])
+# alldata['transformer_eucli_error'] = np.sqrt((alldata['gazed_x'] - alldata['transformer_est_x']) ** 2 + (
+#          alldata['gazed_y'] - alldata['transformer_est_y']) ** 2)
+# alldata['transformer_ang_error'] = alldata.apply(lambda row: compute_angle(row, 'transformer'), axis=1)
+#
+# #alldata['chong_eucli_error'] = np.sqrt( (alldata['gazed_x']-alldata['chong_est_x'])**2 + (alldata['gazed_y']-alldata['chong_est_y'])**2 )
+# #alldata['chong_ang_error'] = alldata.apply(lambda row: compute_angle(row, 'chong'), axis=1)
+#
+# alldata['center_est_x'] = .5
+# alldata['center_est_y'] = .5
+# alldata['center_eucli_error'] =  np.sqrt((alldata['gazed_x'] - .5) ** 2 + (
+#          alldata['gazed_y'] - .5) ** 2)
+# alldata['center_ang_error'] = alldata.apply(lambda row: compute_angle(row, 'center'), axis=1)
+#
+#
+# plotdata = alldata[['condition','transformer_eucli_error','center_eucli_error']]
+# plotdata = pd.melt(plotdata,id_vars=['condition'],
+#                    value_name='error')
+#
+# sns_setup(sns)
+# model = ols('error ~ C(condition)*C(variable)', data=plotdata).fit()
+# ax = sns.barplot(data=plotdata, x='condition', y='error',hue='variable')
+# ax.set(xlabel='', ylabel='Euclidean Error')
+# change_width(ax,.4)
+# ax.spines['top'].set_color('white')
+# ax.spines['right'].set_color('white')
+# ax.figure.savefig('{}/Figures/error_epoch{}.jpg'.format(datapath,epoch))
+#
+#
+# plotdata = alldata[['condition','transformer_ang_error', 'center_ang_error']]
+# plotdata = pd.melt(plotdata,id_vars=['condition'],
+#                    value_name='error')
+#
+# sns_setup(sns)
+# ax = sns.barplot(data=plotdata, x='condition', y='error', hue='variable')
+# ax.set(xlabel='', ylabel='Angular Error')
+# change_width(ax,.4)
+# ax.spines['top'].set_color('white')
+# ax.spines['right'].set_color('white')
+# ax.figure.savefig('{}/Figures/angerror_epoch{}.jpg'.format(datapath,epoch))
+#
+#
+# # # test on the test images from large dataset
+#  datapath = os.path.abspath(os.curdir)
+#  anno_path = "{}/data/annotations".format(datapath)
+#  test_img_path = "{}/data/test".format(datapath)
+#  test_bbx_path = "{}/data/test_bbox".format(datapath)
+#  fig_path = '{}/model_eval_outputs_small/resnet_epoch{}_eval_outputs'.format(datapath, epoch)
+#  criterion = nn.MSELoss(reduction='mean')
+#  chong_est = pd.read_excel('{}/data/Chong_estimation_test.xlsx'.format(datapath))
+#  output = evaluate_test(anno_path, test_img_path, test_bbx_path, chong_est, criterion,  model, fig_path)
+#  output.to_excel('{}/model_eval_outputs_small/transformer_epoch{}_result.xlsx'.format(datapath, epoch))
+#  analyze_error(output, epoch, filename='figures/model_largedata')
