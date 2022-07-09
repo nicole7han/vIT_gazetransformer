34b2f2f5aa0a1549d7a7333d10885901f70a874e
nicole7han <nicole7han@gmail.com>
2022-07-09 16:31:26 -0700

    Update evaluate_largedataset.py

--- evaluate_models/evaluate_largedataset.py
+++ evaluate_models/evaluate_largedataset.py
@@ -276,8 +276,8 @@ def evaluate_test(anno_path, test_img_path, test_bbx_path, chong_est, criterion,
 
 basepath = '/Users/nicolehan/Documents/Research/gazetransformer'
 model = Gaze_Transformer()
-epoch=230
-checkpoint = torch.load('trainedmodels/model_head_chong_detr/model_epoch{}.pt'.format(epoch), map_location='cpu')
+epoch=182
+checkpoint = torch.load('trainedmodels/model_chong_detr/model_epoch{}.pt'.format(epoch), map_location='cpu')
 plt.plot(checkpoint['train_loss'])
 plt.plot(checkpoint['test_loss'])
 loaded_dict = checkpoint['model_state_dict']
