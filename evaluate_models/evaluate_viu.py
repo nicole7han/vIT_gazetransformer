fe206764cfc49c44bdf00643989315d378b49c49
nicole7han <nicole7han@gmail.com>
2022-07-13 09:56:42 -0700

    update

--- evaluate_models/evaluate_viu.py
+++ evaluate_models/evaluate_viu.py
@@ -7,7 +7,7 @@ from script.matcher import *
 
 basepath = '/Users/nicolehan/Documents/Research/gazetransformer'
 model = Gaze_Transformer()
-epoch=70
+epoch=102
 checkpoint = torch.load('trainedmodels/model_chong_detr/model_epoch{}.pt'.format(epoch), map_location='cpu')
 # plt.plot(checkpoint['train_loss'])
 # plt.plot(checkpoint['test_loss'])
