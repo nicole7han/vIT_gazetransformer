6c741f686241c83688af74c240772d57bfa2a481
nicole7han <nicole7han@gmail.com>
2022-07-25 15:27:02 -0700

    update figures

--- evaluate_models/evaluate_across_cond.py
+++ evaluate_models/evaluate_across_cond.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import glob
 from script.model import *
+import statsmodels.api as sm
 from statsmodels.formula.api import ols
+import pingouin as pg
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 from evaluate_models.utils_fine_tuning import *
 from functions.data_ana_vis import *
 from script.matcher import *
@@ -10,7 +13,6 @@ setpallet = sns.color_palette("Set2")
 
 basepath = '/Users/nicolehan/Documents/Research/gazetransformer'
 # model = Gaze_Transformer()
-epoch=108
 # checkpoint = torch.load('trainedmodels/model_chong_detr/model_epoch{}.pt'.format(epoch), map_location='cpu')
 # # plt.plot(checkpoint['train_loss'])
 # # plt.plot(checkpoint['test_loss'])
@@ -27,20 +29,22 @@ Trained_cond = 'Head'
 outpath = '{}/model_eval_viu_outputs/Trained_{}'.format(basepath,Trained_cond)
 
 '''transformer results'''
-results = glob.glob('{}/*{}_result.xlsx'.format(outpath,epoch))
 transformer = pd.DataFrame()
-for f in results:
-    df = pd.read_excel(f)
-    if 'TEST_intact' in f: Test_cond = 'intact'
-    elif 'TEST_nb' in f: Test_cond = 'floating heads'
-    elif 'TEST_nh' in f: Test_cond = 'headless bodies'
+for epoch in [108,60,50]:
+    results = glob.glob('{}/*{}_result.xlsx'.format(outpath,epoch))
+    for f in results:
+        df = pd.read_excel(f)
+        if 'TEST_intact' in f: Test_cond = 'intact'
+        elif 'TEST_nb' in f: Test_cond = 'floating heads'
+        elif 'TEST_nh' in f: Test_cond = 'headless bodies'
 
-    df = df.groupby('image').mean().reset_index()  # compute estimation for each image
-    df['test_cond'] = Test_cond
-    transformer = pd.concat([transformer,df])
+        df = df.groupby('image').mean().reset_index()  # compute estimation for each image
+        df['test_cond'] = Test_cond
+        transformer = pd.concat([transformer,df])
 
 image_info = transformer[['image','gazed_x','gazed_y']].drop_duplicates()
 transformer['Euclidean_error'] = np.sqrt( (transformer['gazed_x']-transformer['transformer_est_x'])**2 + (transformer['gazed_y']-transformer['transformer_est_y'])**2 )
+transformer = transformer.groupby(['image','test_cond']).mean().reset_index()
 transformer = transformer[['test_cond','Euclidean_error']]
 transformer['model'] = 'transformer'
 
@@ -87,11 +91,34 @@ humans['model'] = 'humans'
 plot_data = pd.concat([transformer, cnn, humans])
 plot_data['test_cond'] = plot_data['test_cond'].astype('category')
 plot_data['test_cond'].cat.reorder_categories(['intact', 'floating heads', 'headless bodies'], inplace=True)
-sns_setup_small(sns, (9,7))
+plot_data.to_excel('data/{}_summary.xlsx'.format(Trained_cond), index=None)
+aov = pg.anova(dv='Euclidean_error', between=['model', 'test_cond'], data=plot_data,
+             detailed=True)
+print(aov)
+postdoc =plot_data.pairwise_ttests(dv='Euclidean_error',
+                                   between=['model', 'test_cond'],
+                                   padjust='fdr_bh',
+                                   parametric=True).round(3)
+sig_results = postdoc[postdoc['p-corr']<0.05]
+
+from statannot import add_stat_annotation
+sns_setup_small(sns, (8,6))
 ax = sns.barplot(data = plot_data, x = 'model', y = 'Euclidean_error', hue='test_cond')
 ax.set(xlabel='', ylabel='Euclidean Error', title='Transformer Trained: {}'.format(Trained_cond))
 ax.spines['top'].set_color('white')
 ax.spines['right'].set_color('white')
-ax.legend(title='Test condition', frameon=False,loc='upper left')
-ax.figure.savefig("figures/{}_{}_model_comparison.jpg".format(Trained_cond, epoch), bbox_inches='tight')
+box_pairs_model = [('cnn','transformer'),('humans','transformer')]
+# ps_models = [0.001, 0.001]
+# add_stat_annotation(ax, data=plot_data, x = 'model', y = 'Euclidean_error',
+#                     box_pairs= box_pairs_model, perform_stat_test=False, pvalues=ps_models,
+#                     loc='outside',line_offset=0.015, line_offset_to_box=0.005, verbose=2)
+box_pairs = [(('cnn','headless bodies'),('cnn','floating heads')),(('cnn','headless bodies'),('cnn','intact')),
+             (('humans','headless bodies'),('humans','floating heads')),(('humans','headless bodies'),('humans','intact')),
+             (('transformer','floating heads'),('transformer','intact')),(('transformer','floating heads'),('transformer','headless bodies'))]
+ps = [0.001, 0.001,0.001, 0.001,0.001, 0.001]
+add_stat_annotation(ax, data=plot_data, x = 'model', y = 'Euclidean_error', hue='test_cond',
+                    box_pairs= box_pairs, perform_stat_test=False, pvalues=ps,
+                    loc='inside',line_offset=0.1, line_offset_to_box=0.005, verbose=0)
+ax.legend(title='Test condition', loc='upper left', frameon=False)
+ax.figure.savefig("figures/{}_model_comparison.jpg".format(Trained_cond), bbox_inches='tight')
 plt.close()
