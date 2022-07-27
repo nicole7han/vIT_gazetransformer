0842e2834e59362db83fe336ca93729888767ac2
nicole7han <nicole7han@gmail.com>
2022-07-27 11:30:01 -0700

    Update paper_figures.py

--- evaluate_models/paper_figures.py
+++ evaluate_models/paper_figures.py
@@ -356,3 +356,58 @@ ax.legend(frameon=False)
 ax.figure.savefig("figures/allcorr_intact.png", dpi=300, bbox_inches='tight')
 plt.close()
 
+
+
+''' Human, CNN, 3 Transformers performance on intact image '''
+plot_data = results.copy()
+plot_data = plot_data[plot_data['test_cond']=='intact']
+humans = plot_data[(plot_data['model']=='Humans') & (plot_data['train_cond']=='Head')]
+CNN = plot_data[(plot_data['model']=='CNN') & (plot_data['train_cond']=='Head')]
+transformers = plot_data[(plot_data['model']=='Transformer')]
+def label_transformer(row):
+    return row['train_cond'] + ' Transformer'
+transformers['model'] = transformers.apply(label_transformer, axis=1)
+plot_data = pd.concat([humans,CNN,transformers])
+plot_data['model'] = plot_data['model'].astype('category')
+plot_data['model'] = plot_data['model'].cat.rename_categories(['Humans', 'CNN', 'HeadBody Gazetransformer',
+                                                               'Head Gazetransformer','Body Gazetransformer',])
+plot_data = plot_data[['Euclidean_error','Angular_error','model']]
+# plot_data = plot_data.melt(id_vars='model')
+# aov = pg.anova(dv='Euclidean_error', between=['model'], data=plot_data,
+#              detailed=True)
+# print(aov)
+# postdoc =plot_data.pairwise_ttests(dv='Euclidean_error',
+#                                    between=['model'],
+#                                    padjust='fdr_bh',
+#                                    parametric=True).round(3)
+# sig_results = postdoc[postdoc['p-corr']<0.05]
+# box_pairs = [(('CNN','headless bodies'),('CNN','floating heads')),(('CNN','headless bodies'),('CNN','intact')),
+#              (('Humans','headless bodies'),('Humans','floating heads')),(('Humans','headless bodies'),('Humans','intact')),
+#              (('Head Gazetransformer','floating heads'),('Head Gazetransformer','intact')), (('Head Gazetransformer','floating heads'),('Head Gazetransformer','headless bodies'))]
+# ps = [0.001, 0.001,0.001, 0.001,0.001,0.001]
+
+sns_setup_small(sns, (8,6))
+error = 'Euclidean_error'
+ax = sns.barplot(data = plot_data, x = error, y = 'model',color=setpallet[0])
+ax.set(xlabel='Euclidean Error', ylabel='')
+ax.spines['top'].set_color('white')
+ax.spines['right'].set_color('white')
+# add_stat_annotation(ax, data=plot_data, x = 'model', y = 'Euclidean_error', hue='test_cond',
+#                     box_pairs= box_pairs, perform_stat_test=False, pvalues=ps,
+#                     loc='outside', verbose=2)
+# ax.legend(title='test condition', loc='upper right', frameon=False, bbox_to_anchor=[1.4, 0.9])
+ax.figure.savefig("figures/Euclidean_human_allmodels_intact.png", dpi=300, bbox_inches='tight')
+plt.close()
+
+sns_setup_small(sns, (8,6))
+error = 'Angular_error'
+ax = sns.barplot(data = plot_data, x = error, y = 'model',color=setpallet[1])
+ax.set(xlabel='Angular Error', ylabel='')
+ax.spines['top'].set_color('white')
+ax.spines['right'].set_color('white')
+# add_stat_annotation(ax, data=plot_data, x = 'model', y = 'Euclidean_error', hue='test_cond',
+#                     box_pairs= box_pairs, perform_stat_test=False, pvalues=ps,
+#                     loc='outside', verbose=2)
+# ax.legend(title='test condition', loc='upper right', frameon=False, bbox_to_anchor=[1.4, 0.9])
+ax.figure.savefig("figures/Angular_human_allmodels_intact.png", dpi=300, bbox_inches='tight')
+plt.close()
