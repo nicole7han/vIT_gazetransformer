91d684572c60faefe426dec05ae94a853d48489f
nicole7han <nicole7han@gmail.com>
2022-08-10 07:01:47 -0700

    update

--- evaluate_models/evaluate_viu.py
+++ evaluate_models/evaluate_viu.py
@@ -40,8 +40,10 @@ for epoch in [130, 160, 190]:
             gazer_bbox = 'b'
         fig_path='{}/transformer_TEST_{}_epoch{}'.format(outpath,cond,epoch)
 
-        matcher = build_matcher(set_cost_class=1, set_cost_bbox=5, set_cost_giou=2)
-        weight_dict = {'loss_ce': 1, 'loss_bbox': 20, 'loss_giou': 2}
+#        matcher = build_matcher(set_cost_class=1, set_cost_bbox=5, set_cost_giou=2)
+#        weight_dict = {'loss_ce': 1, 'loss_bbox': 20, 'loss_giou': 2}
+        matcher = build_matcher(set_cost_class=1, set_cost_bbox=1, set_cost_giou=2)
+        weight_dict = {'loss_ce': 10, 'loss_bbox':1, 'loss_giou': 2}
         losses = ['labels', 'boxes']
         num_classes = 1
         criterion = SetCriterion(num_classes, matcher=matcher, weight_dict=weight_dict,
