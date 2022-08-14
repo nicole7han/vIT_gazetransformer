f712c4d0e98bbde21f58f271289ab0b3a01f5b7c
nicole7han <nicole7han@gmail.com>
2022-08-14 08:52:06 -0700

    update

--- script/main_imageclips.py
+++ script/main_imageclips.py
@@ -58,7 +58,7 @@ test_bbx_path = "{}/data/test_bbox".format(basepath)
 # cleanup_dataset(segmask_path, bbx_path, img_path)
 
 model = Gaze_Transformer()
-epoch = 170
+epoch = 260
 checkpoint = torch.load('trainedmodels/model_chong_detr/model_epoch{}.pt'.format(epoch), map_location='cpu')
 plt.plot(checkpoint['train_loss'][6:])
 plt.plot(checkpoint['test_loss'][6:])
