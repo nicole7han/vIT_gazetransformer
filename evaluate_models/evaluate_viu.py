f9aad848daadaced2ba7e1c7d107fd62609f8d4b
nicole7han <nicole7han@gmail.com>
2022-08-15 15:50:27 -0700

    update path

--- evaluate_models/evaluate_viu.py
+++ evaluate_models/evaluate_viu.py
@@ -7,7 +7,7 @@ from script.matcher import *
 
 basepath = '/Users/nicolehan/Documents/Research/gazetransformer'
 model = Gaze_Transformer()
-for epoch in [50]:
+for epoch in [20]:
     # epoch=300,100,340
     checkpoint = torch.load('trainedmodels/model_headbody_chong_vit/model_epoch{}.pt'.format(epoch), map_location='cpu')
 #    plt.plot(checkpoint['train_loss'][6:])
@@ -26,7 +26,7 @@ for epoch in [50]:
 
     # evaluate both models' estimation on viu dataset
     datapath = "{}/gaze_video_data".format(basepath)
-    outpath = '{}/model_eval_viu_outputs/Trained_HeadBody'.format(basepath)
+    outpath = '{}/model_eval_viu_outputs/Trained_HeadBody_VIT'.format(basepath)
     os.makedirs(outpath, exist_ok=True)
     anno_path = '{}/Video_Info.xlsx'.format(datapath)
     for cond in ['intact','nb','nh']: #'intact','nb','nh'
